90155181d536e4399a6a660a91cbb6540aba29d3
Daniel Pfeifer <daniel@pfeifer-mail.de>
2025-07-10 09:55:04 +0200

    Add CMake project skeleton

---
+++ CTestConfig.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2025-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+set(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
+set(CTEST_SUBMIT_URL "https://chorse.dev/api/submit?project=btck")
